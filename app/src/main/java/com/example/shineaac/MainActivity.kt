7543bc91d1d0ce71c34787613e500593f8311972
Lîm Lip <poi890poi@gmail.com>
2024-07-23 00:48:59 +0800

    Implemented SAC text input.

--- app/src/main/java/com/example/shineaac/MainActivity.kt
+++ app/src/main/java/com/example/shineaac/MainActivity.kt
@@ -1,24 +1,50 @@
 package com.example.shineaac
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.view.MotionEvent
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+
+const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     private val handler = Handler(Looper.getMainLooper())
-    private val scanInterval: Long = 400 // Change color every 2 seconds
+    private val scanInterval: Long = 750 // Change color every 2 seconds
     private var sacLevel = 0
-    private var sacActive = -1
-    private var sacPrev = -1
+    private var sacActive = arrayOf(-1, -1, -1)
+    private var sacPrev = arrayOf(-1, -1, -1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        onTouchHandler()
         startSwitchAccessScan()
+    }
+
+    private fun onTouchHandler() {
+        val sacTableLayout = findViewById<TableLayout>(R.id.sac_table_layout)
+        sacTableLayout.setOnTouchListener { v, event ->
+            Log.v(TAG, "event=" + event);
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Handle touch down event
+                    sacLevel++
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    // Handle touch up event
+                    v.performClick()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun startSwitchAccessScan() {
@@ -39,22 +65,78 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun switchAccessScan() {
+    private fun sacInput() {
+        Log.v(TAG, "sac input " + sacActive[0] + ", " + sacActive[1])
         val sacTableLayout = findViewById<TableLayout>(R.id.sac_table_layout)
+        val row = sacTableLayout.getChildAt(sacActive[0]) as TableRow
+        val cell = row.getChildAt(sacActive[1]) as TextView
+        val sacInput = findViewById<TextView>(R.id.sac_input)
+        val txt = cell.text.toString()
+        when (txt) {
+            getString(R.string.symbol_clear) -> {
+                sacInput.setText("")
+            }
+            getString(R.string.symbol_space) -> {
+                sacInput.append(" ")
+            }
+            getString(R.string.symbol_backspace) -> {
+                sacInput.setText(sacInput.text.substring(0, sacInput.text.length - 1))
+            }
+            getString(R.string.symbol_question) -> {
+                sacInput.append("?")
+            }
+            else -> {
+                sacInput.append(txt)
+            }
+        }
+        sacLevel = 0
+        sacActive[0] = -1
+        sacActive[1] = -1
+    }
+
+    private fun sacLvl1() {
+        val sacTableLayout = findViewById<TableLayout>(R.id.sac_table_layout)
+        val row = sacTableLayout.getChildAt(sacActive[0]) as TableRow
         while (true) {
-            if (sacActive >= sacTableLayout.childCount) sacActive = 0
-            else sacActive++
-            val active = sacTableLayout.getChildAt(sacActive)
-            if (active is TableRow) {
-                setRowBackgroundColor(active, getColor(R.color.purple_200))
+            if (sacActive[1] >= row.childCount) sacActive[1] = 0
+            else sacActive[1]++
+            val cell = row.getChildAt(sacActive[1])
+            if (cell is TextView) {
+                cell.setBackgroundColor(getColor(R.color.teal_700))
                 break
             }
         }
-        if (sacPrev >= 0) {
-            val prev = sacTableLayout.getChildAt(sacPrev)
+        if (sacPrev[1] >= 0) {
+            val prev = row.getChildAt(sacPrev[1])
+            prev.setBackgroundColor(getColor(R.color.purple_200))
+        }
+        sacPrev[1] = sacActive[1]
+    }
+
+    private fun sacLvl0() {
+        val sacTableLayout = findViewById<TableLayout>(R.id.sac_table_layout)
+        while (true) {
+            if (sacActive[0] >= sacTableLayout.childCount) sacActive[0] = 0
+            else sacActive[0]++
+            val row = sacTableLayout.getChildAt(sacActive[0])
+            if (row is TableRow) {
+                setRowBackgroundColor(row, getColor(R.color.purple_200))
+                break
+            }
+        }
+        if (sacPrev[0] >= 0) {
+            val prev = sacTableLayout.getChildAt(sacPrev[0])
             if (prev is TableRow) setRowBackgroundColor(prev, getColor(R.color.ivory))
         }
-        sacPrev = sacActive
+        sacPrev[0] = sacActive[0]
+    }
+
+    private fun switchAccessScan() {
+        when(sacLevel) {
+            0 -> sacLvl0()
+            1 -> sacLvl1()
+            2 -> sacInput()
+        }
     }
 
     override fun onDestroy() {
